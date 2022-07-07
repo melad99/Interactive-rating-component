48d1ae3f1a2690093c984f2424c9b3c1be78c90e
melad99 <melad1999@gmail.com>
2022-07-07 00:54:35 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+function thanks(){
+document.getElementById("thanks-card-boxx").style.display="block";
+}
+
+function show1() {
+	document.getElementById("rate1").style.display = "block";
+	
+}
+function show2() {
+	document.getElementById("rate2").style.display = "block";
+}
+function show3() {
+	document.getElementById("rate3").style.display = "block";
+}
+function show4() {
+	document.getElementById("rate4").style.display = "block";
+}
+function show5() {
+	document.getElementById("rate5").style.display = "block";
+}
+
